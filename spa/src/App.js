e63ce5920d41e263c9838a2cbfa7dfbbc4a81fd8
LeeEugene1 <uwm1004@gmail.com>
2023-03-01 19:36:14 +0900

    feat: pages setting

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,12 +1,19 @@
 import Header from "./components/Header.js"
+import Homepage from "./Homepage.js"
+import SignupPage from './SignupPage.js'
 
 class App{
-    constructor($app){
-        this.$app = $app
+    constructor($body){
+        this.$body = $body
         this.render()
     }
     render(){
-        new Header(this.$app)
+        new Header(this.$body)
+        const main = document.createElement('main')
+        main.setAttribute('id', 'content_page')
+        this.$body.appendChild(main)
+        new SignupPage(main)
+        new Homepage(main)
     }
 }
 export default App
