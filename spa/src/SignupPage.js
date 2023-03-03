ac8d69180f02e91c4bceac5873ac4c55caf5cf9d
LeeEugene1 <uwm1004@gmail.com>
2023-03-03 20:31:31 +0900

    feat: pages, regex

--- spa/src/SignupPage.js
+++ spa/src/SignupPage.js
@@ -8,6 +8,46 @@ class SignupPage{
     render(){
         const title = new ContentTitle(this.$main, 'SignupPage')
         title.render()
+
+        this.$main.innerHTML = `
+        <div class="content_title">
+        <h1> Hello, GreatPeoPle!</h1>
+            </div>
+            <div id="form_container">
+                <span class="form_elem">
+                    <input id="name" placeholder="이름">
+                </span>
+                <span class="form_elem">
+                    <input id="email" placeholder="이메일">
+                </span>
+                <span class="form_elem">
+                    <select id="role" name="role">
+                        <option value="">직군을 선택해주세요</option>
+                        <option value="backend">백엔드</option>
+                        <option value="frontend">프론트엔드</option>
+                        <option value="fullstack">풀스택</option>
+                    </select>
+                </span>
+                <span class="form_elem">
+                    <button type="submit">등록</button>
+                </span>
+            </div>
+        `
+
+        const name = document.querySelector('#name')
+
+        document.querySelector('button').addEventListener('click',()=>{
+            if(name.value === ''){
+                alert('채우삼')
+                return false;
+            }
+            const nameRegex = /^[가-힣]{2,4}$/
+            if(!nameRegex.test(name.value)){
+                alert('정규식틀림')
+                return false;
+            }
+            alert('성공!')
+        })
     }
 }
 export default SignupPage;
